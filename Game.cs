e8947818648cbe3f34c6c4407303af236635da70
AJFirefox <AJFirefox08@gmail.com>
2026-04-06 08:49:42 -0500

    done

---
+++ Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_Price_Austin
+{
+    internal class Game
+    {
+        // Public fields
+        public string title;
+        public string genre;
+
+        // Constructor with default values
+        public Game(string title = "Unknown Title", string genre = "Casual")
+        {
+            this.title = title;
+            this.genre = genre;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create and initialize a Game object
+            Game game1 = new Game("The Legend of Zelda", "Adventure");
+
+            Console.WriteLine("Title: " + game1.title);
+            Console.WriteLine("Genre: " + game1.genre);
+            Console.WriteLine();
+
+            // Create a Game object with default values
+            Game game2 = new Game();
+
+            Console.WriteLine("Title: " + game2.title);
+            Console.WriteLine("Genre: " + game2.genre);
         }
     }
 }
